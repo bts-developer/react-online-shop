7a4971b0aa13c20b2aa08d48fcd56b392f75460f
bts-developer <bts.developer2019@gmail.com>
2020-02-04 12:13:35 +0330

    Updates

---
+++ precache-manifest.168e9f9f7643238dadc0a661bc090d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab20a0fa41689f1a58ef869e7eb3115",
+    "url": "/react-online-shop/index.html"
+  },
+  {
+    "revision": "3ac325d790d0674b0097",
+    "url": "/react-online-shop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bc20cf83653f6691b10",
+    "url": "/react-online-shop/static/js/2.11d20984.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-online-shop/static/js/2.11d20984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac325d790d0674b0097",
+    "url": "/react-online-shop/static/js/main.857beaf5.chunk.js"
+  },
+  {
+    "revision": "abaecd7851297059b77d",
+    "url": "/react-online-shop/static/js/runtime-main.b8a847ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-online-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
