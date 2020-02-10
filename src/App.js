d426a566ef4ca7abc70dfead97fc258d59b02e24
bts-developer <bts.developer2019@gmail.com>
2020-02-10 17:14:49 +0330

    cart item component created

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import setCurrentUser from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unSubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShop => {
-          this.setState({
-            currentUser: {
-              id: snapShop.id,
-              ...snapShop.data()
-            }
+          setCurrentUser({
+            id: snapShop.id,
+            ...snapShop.data()
           });
         });
       }
-      this.setState({ currentUser: userAuth });
+
+      console.log(this.state);
+      setCurrentUser(userAuth);
     });
   }
 
@@ -42,15 +37,23 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUp} />
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUp/>)} />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import "./collection-preview.styles.scss";
+
 import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{title}</h1>
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map(({ ...itemsProps }) => (
-        <CollectionItem {...itemsProps} />
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
